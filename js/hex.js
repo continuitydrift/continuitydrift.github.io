f34feee30776f8f30dfbc7009378db09e7e57459
Chad Hines <chad@newlanguagestudio.com>
2016-04-24 09:40:44 -0500

    starting over based on new template
    Rather than having the website as a whole be  a kind of live-streaming
    of building something new from scratch, a much better tactic would be to
    display functional skills of adapting an existing template.

---
+++ js/hex.js
@@ -0,0 +1,11 @@
+var main = function() {
+
+/*this should put hex 2 in the absolute center of the page
+*/
+
+$('container').addClass('Absolute-Center');
+
+/*$('.arrow-next').click(function() {
+};
+*/
+};
